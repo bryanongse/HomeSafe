368526b3ce64b1b88df04f97ed718d565b43d548
Daniel Yang <danielyang@Daniels-MacBook-Pro.local>
2022-10-16 06:05:29 -0700

    edits

--- index.ts
+++ index.ts
@@ -85,14 +85,6 @@ function initMap(): void {
         return;
       }
 
-      const icon = {
-        url: place.icon as string,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25),
-      };
-
       // Create a marker for each place.
       start.push(
         new google.maps.Marker({
@@ -134,14 +126,6 @@ function initMap(): void {
         console.log("Returned place contains no geometry");
         return;
       }
-
-      const icon = {
-        url: place.icon as string,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25),
-      };
 
       // Create a marker for each place.
       end.push(
